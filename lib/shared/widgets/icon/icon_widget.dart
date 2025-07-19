c43f8f5bcf3133b6670d023e16b9a3075d4a5bcf
SangeethCK <sangeethck7@gmail.com>
2025-07-19 14:07:55 +0530

    BUG FIXES IN CHAT

--- lib/shared/widgets/icon/icon_widget.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-Widget iconWidget(double? height) {
-  return Icon(Icons.photo, size: height ?? 65.h, color: Colors.grey);
-}
